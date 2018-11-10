c383e7a68bd41b47bde961efc7e3ce8df1d38409
campbell-thinkingcap <campbell@thinkingcap.com>
2018-11-10 11:17:17 -0500

    Add project files.

---
+++ TCMobile/TCMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TCMobile.Models;
+
+namespace TCMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
