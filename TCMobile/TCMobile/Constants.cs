75e5ba0586d19044ef00ed26fd3a0c4f50fcc82a
campbell-thinkingcap <campbell@thinkingcap.com>
2018-12-14 12:36:41 -0500

    changed catalogue url to https

--- TCMobile/TCMobile/Constants.cs
+++ TCMobile/TCMobile/Constants.cs
@@ -6,7 +6,7 @@ namespace TCMobile
 {
     class Constants
     {
-        public static string StudentCatalogue = "http://stable.thinkingcap.com/Mobile/Catalogue.ashx";
+        public static string StudentCatalogue = "https://stable.thinkingcap.com/Mobile/Catalogue.ashx";
         public static string StudentID = "740ad541-661a-43ea-b825-cec3788f2c3a";
         public static string ProgramID = "00000000-0000-0000-0000-000000000000";
         
