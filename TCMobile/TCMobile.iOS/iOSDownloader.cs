c0b2b28bb4a026586223580c184292a5a3e4f692
campbell-thinkingcap <campbell@thinkingcap.com>
2018-12-17 19:58:00 -0500

    implementin IDownloader of ios

---
+++ TCMobile/TCMobile.iOS/iOSDownloader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.ComponentModel;
+
+using Foundation;
+using UIKit;
+
+namespace TCMobile.iOS
+{
+    class iOSDownloader : IDownloader
+    {
+        public event EventHandler<DownloadEventArgs> OnFileDownloaded;
+
+        public void DownloadFile(string url, string folder)
+        {
+            string pathToNewFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), folder);
+            Directory.CreateDirectory(pathToNewFolder);
+
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                string pathToNewFile = Path.Combine(pathToNewFolder, Path.GetFileName(url));
+                webClient.DownloadFileAsync(new Uri(url), pathToNewFile);
+            }
+            catch (Exception ex)
+            {
+                if (OnFileDownloaded != null)
+                    OnFileDownloaded.Invoke(this, new DownloadEventArgs(false));
+            }
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                if (OnFileDownloaded != null)
+                    OnFileDownloaded.Invoke(this, new DownloadEventArgs(false));
+            }
+            else
+            {
+                if (OnFileDownloaded != null)
+                    OnFileDownloaded.Invoke(this, new DownloadEventArgs(true));
+            }
+        }
+    }
+}
